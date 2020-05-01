5c93adc98f2dcb393f551bc7cd626eab2839cbfb
Ben Barnes <bm.barnes@setonhill.edu>
2020-05-01 18:05:14 -0400

    Ready!

--- Program8/ViewController.swift
+++ Program8/ViewController.swift
@@ -10,9 +10,85 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var imageOne: UIImageView!
+    
+    
+    @IBOutlet weak var imageTwo: UIImageView!
+    
+    
+    @IBOutlet weak var imageThree: UIImageView!
+    
+    
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    @IBAction func foundTap(_ sender: Any) {
+        // On tap show image
+        imageOne.isHidden = false
+              imageOne.startAnimating()
+        Label.text = "Now swipe"
+               
+           // Hide other images
+               if imageOne.isAnimating || imageTwo.isAnimating
+               {
+                   imageOne.isHidden = true
+                   imageTwo.isHidden = true
+               }
+
+    }
+    
+    
+    @IBAction func foundSwipe(_ sender: Any) {
+        imageTwo.isHidden = false
+               imageTwo.startAnimating()
+               Label.text = "Now shake"
+               
+           // Hide all other images
+               if imageOne.isAnimating || imageThree.isAnimating
+               {
+                   imageOne.isHidden = true
+                   imageThree.isHidden = true
+               }
+        
+
+    }
+    
+    // On shake change to image 3
+       override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+           
+           if motion==UIEvent.EventSubtype.motionShake {
+               imageThree.isHidden = false
+              imageThree.startAnimating()
+               Label.text = "That was cool!"
+              
+       // Hide other images
+               if imageOne.isAnimating || imageTwo.isAnimating
+               {
+                   imageOne.isHidden = true
+                   imageTwo.isHidden = true
+               }
+           }
+       }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        let imageOneAnimation: [UIImage] = [UIImage(named: "image1")!,]
+               imageOne.animationImages = imageOneAnimation
+               
+               let imageTwoAnimation: [UIImage] = [UIImage(named: "image2")!,]
+               imageTwo.animationImages = imageTwoAnimation
+               
+               let imageThreeAnimation: [UIImage] = [UIImage(named: "image3")!,]
+              imageThree.animationImages = imageThreeAnimation
+        
+
     }
 
 

9e9880c540d21d0abcb17911585c6177f335e1d0
Ben Barnes <bm.barnes@setonhill.edu>
2020-05-01 17:32:15 -0400

    Initial Commit

---
+++ Program8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Program8
+//
+//  Created by ben barnes on 5/1/20.
+//  Copyright © 2020 ben barnes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
